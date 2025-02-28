9b43555f354552e1a5934a0218b01378d96f08db
Yuvraj Sharma <galaxy0.07.04@gmail.com>
2025-02-28 10:18:20 +0000

    Clerk Fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,31 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+  if (isPublicRoute(req)) {
+    return NextResponse.next(); // Allow access to sign-in and sign-up pages
+  }
+
+  if (isProtectedRoute(req)) {
+    try {
+      await auth.protect(); // Protect other routes
+      return NextResponse.next();
+    } catch (error) {
+      //auth.protect() throws an error when not authenticated, so we redirect to sign in here.
+      const signInUrl = new URL("/sign-in", req.url);
+      signInUrl.searchParams.set("redirect_url", req.url);
+      return NextResponse.redirect(signInUrl);
+    }
+  }
+  return NextResponse.next();
 });
 
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-
     "/(api|trpc)(.*)",
   ],
 };
