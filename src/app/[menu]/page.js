d8467ff5b71c28b39c496994be117e128b53b401
Craig Clayton <craig__clayton@outlook.com>
2025-02-28 20:33:50 +0000

    edit utils and optional chaining for id in menu/id route so it doesn't throw an error

--- src/app/[menu]/page.js
+++ src/app/[menu]/page.js
@@ -1,51 +1,50 @@
-import { db } from "@/utils/dbConnection";
-import Link from "next/link";
-import Image from "next/image";
+import { db } from '@/utils/dbConnection';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default async function MenuPage({ params }) {
-  console.log("params in whole are", await params);
+  console.log('params in whole are', await params);
   const menuparams = (await params).menu;
-  console.log("menu params are", menuparams);
+  console.log('menu params are', menuparams);
   const menu = await db.query(`select * from menu where title_name = $1`, [
     menuparams,
   ]);
-  console.log("individual menu is", menu);
-  const menuRow = menu.rows[0].id;
+  console.log('individual menu is', menu);
+  const menuRow = menu.rows[0]?.id;
   const business = await db.query(`select * from business where menu_id = $1`, [
     menuRow,
   ]);
-  console.log("all business results", business);
+  console.log('all business results', business);
   const wrangleData = business.rows;
   console.log(wrangleData);
 
   return (
-    <div className="h-auto m-6 mb-50 bg-white">
-      <h1 className="items-center text-center capitalize text-black text-[40px] font-bold">
+    <div className='h-auto m-6 mb-50 bg-white'>
+      <h1 className='items-center text-center capitalize text-black text-[40px] font-bold'>
         {menuparams}
       </h1>
 
-     
       <div>
-        <div className="bg-white flex flex-row overflow-scroll">
-          {wrangleData.length > 0 ? "" : "Sorry! There is no Business to show"}
+        <div className='bg-white flex flex-row overflow-scroll'>
+          {wrangleData.length > 0 ? '' : 'Sorry! There is no Business to show'}
           {wrangleData.map((data) => (
             <div
-              className="flex flex-col items-center gap-4 m-6 p-6 bg-gray-100"
+              className='flex flex-col items-center gap-4 m-6 p-6 bg-gray-100'
               key={data.id}
             >
-              <div className="w-[250px] h-[250px]">
+              <div className='w-[250px] h-[250px]'>
                 <Image
-                  className="h-full w-full object-cover"
+                  className='h-full w-full object-cover'
                   src={data.business_img}
-                  alt="Business"
+                  alt='Business'
                   width={250}
                   height={250}
                   unoptimized
                 />
               </div>
-              <h1 className="text-gray-800 bg-white">{data.business_name}</h1>
+              <h1 className='text-gray-800 bg-white'>{data.business_name}</h1>
               <Link
-                className="text-gray-800 text-center text-sm bg-white p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110"
+                className='text-gray-800 text-center text-sm bg-white p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110'
                 href={`/foods/${data.id}`}
               >
                 Discover More!
@@ -54,8 +53,6 @@ export default async function MenuPage({ params }) {
           ))}
         </div>
       </div>
-
-   
     </div>
   );
 }
--- src/utils/dbConnection.js
+++ src/utils/dbConnection.js
@@ -1,7 +1,6 @@
-import pg from "pg";
-
-const dbConnection = process.env.NEXT_PUBLIC_DATABASE_URL;
+import pg from 'pg';
+const dbConnectionString = process.env.NEXT_PUBLIC_DATABASE_URL;
 
 export const db = new pg.Pool({
-    connectionString:dbConnection
+  connectionString: dbConnectionString,
 });
