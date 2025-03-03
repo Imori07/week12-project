bddf4a17e86f22a57762dcfcac494e74ba2dd6fc
Yurii <pavlenkoyura88@gmail.com>
2025-03-03 10:59:18 +0000

    sign-up and sing-in styling

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -1,9 +1,20 @@
 import { SignIn } from "@clerk/nextjs";
+
 export default function SignInPage() {
   return (
-    <>
-      <h2>Sign in, please!</h2>
-      <SignIn />
-    </>
+    <div
+      className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url('https://cathedralquarternorwich.co.uk/wp-content/uploads/elementor/thumbs/quayside-norwich-wensum-river-walk-oyy0gfat2401578fzznrk0c2zsnbq3ib2h8m870arg.jpg')",
+      }}
+    >
+      <div className="bg-white bg-opacity-90 p-8 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold text-gray-900 text-center mb-4">
+          Sign in, please!
+        </h2>
+        <SignIn />
+      </div>
+    </div>
   );
 }
