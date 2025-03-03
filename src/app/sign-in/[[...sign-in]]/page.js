2cc395bdc4beba72fe439df88b02fbebd7c7b4c7
Yurii <pavlenkoyura88@gmail.com>
2025-03-03 11:09:28 +0000

    sign-in background change

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -6,7 +6,7 @@ export default function SignInPage() {
       className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center"
       style={{
         backgroundImage:
-          "url('https://cathedralquarternorwich.co.uk/wp-content/uploads/elementor/thumbs/quayside-norwich-wensum-river-walk-oyy0gfat2401578fzznrk0c2zsnbq3ib2h8m870arg.jpg')",
+          "url('https://twotravelingtexans.com/wp-content/uploads/2019/06/riverside-wide.jpg')",
       }}
     >
       <div className="bg-white bg-opacity-90 p-8 rounded-lg shadow-lg">
