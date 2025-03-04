d9e2c829b20629d659d32adb1c56f6fb5072db0e
Craig Clayton <craig__clayton@outlook.com>
2025-03-04 14:22:26 +0000

    disable if pending

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -5,7 +5,7 @@ import { useFormStatus } from 'react-dom';
 const SubmitButton = ({ text, classNames }) => {
   const { pending } = useFormStatus();
   return (
-    <button type='submit' className={classNames}>
+    <button type='submit' className={classNames} disabled={pending}>
       {pending ? 'Submitting...' : text}
     </button>
   );
