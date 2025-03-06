3273966f840527802e3c1529dd4275b2c8569106
Craig Clayton <craig__clayton@outlook.com>
2025-03-06 14:48:30 +0000

    fix styles

--- src/components/businessfilter.jsx
+++ src/components/businessfilter.jsx
@@ -1,24 +1,19 @@
-"use client";
-import { useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
-
-
-
-export default function BusinessFilter({businessId,cafe, Bars, Restaurant}) {
+export default function BusinessFilter({ businessId, cafe, Bars, Restaurant }) {
   const [show, setshow] = useState(0);
-
-
 
   return (
     <>
-      <div className=" flex gap-2 flex-row items-center text-center justify-center ">
+      <div className=' flex gap-2 flex-row items-center text-center justify-center '>
         <button
           onClick={() => {
             setshow(0);
           }}
-          className="bg-amber-500 m-2 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-lg"
+          className='bg-amber-500 m-2 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-lg'
         >
           Foods & Drinks
         </button>
@@ -26,7 +21,7 @@ export default function BusinessFilter({businessId,cafe, Bars, Restaurant}) {
           onClick={() => {
             setshow(1);
           }}
-          className="bg-amber-500 m-2 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-lg"
+          className='bg-amber-500 m-2 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-lg'
         >
           Restaurant
         </button>
@@ -34,7 +29,7 @@ export default function BusinessFilter({businessId,cafe, Bars, Restaurant}) {
           onClick={() => {
             setshow(2);
           }}
-          className="bg-amber-500 m-2 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-lg"
+          className='bg-amber-500 m-2 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-lg'
         >
           Cafe
         </button>
@@ -42,124 +37,113 @@ export default function BusinessFilter({businessId,cafe, Bars, Restaurant}) {
           onClick={() => {
             setshow(3);
           }}
-          className="bg-amber-500 m-2 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-lg"
+          className='bg-amber-500 m-2 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-lg'
         >
           Bars
         </button>
-        </div>
-        <div className='bg-white flex flex-row overflow-scroll'>
-        {show === 0 && (
-            businessId.map((data) => (
-                <div
-                  className='flex flex-col items-center gap-4 m-6 p-6 bg-gray-100'
-                  key={data.id}
-                >
-                  <div className='w-[250px] h-[250px]'>
-                    <Image
-                      className='h-full w-full object-cover'
-                      src={data.business_img}
-                      alt='Business'
-                      width={250}
-                      height={250}
-                      unoptimized
-                    />
-                  </div>
-                  <h1 className='text-gray-800 bg-white'>{data.business_name}</h1>
-                  <Link
-                    className='text-gray-800 text-center text-sm bg-white p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110'
-                    href={`/foods/${data.id}`}
-                  >
-                    Discover More!
-                  </Link>
-                </div>
-              ))
-      
-        )}
-         {show === 1 && (
-            Restaurant.map((data) => (
-                <div
-                  className='flex flex-col items-center gap-4 m-6 p-6 bg-gray-100'
-                  key={data.id}
-                >
-                  <div className='w-[250px] h-[250px]'>
-                    <Image
-                      className='h-full w-full object-cover'
-                      src={data.business_img}
-                      alt='Business'
-                      width={250}
-                      height={250}
-                      unoptimized
-                    />
-                  </div>
-                  <h1 className='text-gray-800 bg-white'>{data.business_name}</h1>
-                  <Link
-                    className='text-gray-800 text-center text-sm bg-white p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110'
-                    href={`/foods/${data.id}`}
-                  >
-                    Discover More!
-                  </Link>
-                </div>
-              ))
-      
-        )}
-         {show === 2 && (
-            cafe.map((data) => (
-                <div
-                  className='flex flex-col items-center gap-4 m-6 p-6 bg-gray-100'
-                  key={data.id}
-                >
-                  <div className='w-[250px] h-[250px]'>
-                    <Image
-                      className='h-full w-full object-cover'
-                      src={data.business_img}
-                      alt='Business'
-                      width={250}
-                      height={250}
-                      unoptimized
-                    />
-                  </div>
-                  <h1 className='text-gray-800 bg-white'>{data.business_name}</h1>
-                  <Link
-                    className='text-gray-800 text-center text-sm bg-white p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110'
-                    href={`/foods/${data.id}`}
-                  >
-                    Discover More!
-                  </Link>
-                </div>
-              ))
-      
-        )}
-         {show === 3 && (
-            Bars.map((data) => (
-                <div
-                  className='flex flex-col items-center gap-4 m-6 p-6 bg-gray-100'
-                  key={data.id}
-                >
-                  <div className='w-[250px] h-[250px]'>
-                    <Image
-                      className='h-full w-full object-cover'
-                      src={data.business_img}
-                      alt='Business'
-                      width={250}
-                      height={250}
-                      unoptimized
-                    />
-                  </div>
-                  <h1 className='text-gray-800 bg-white'>{data.business_name}</h1>
-                  <Link
-                    className='text-gray-800 text-center text-sm bg-white p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110'
-                    href={`/foods/${data.id}`}
-                  >
-                    Discover More!
-                  </Link>
-                </div>
-              ))
-      
-        )}
- 
-    </div>
-
- 
+      </div>
+      <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {show === 0 &&
+          businessId.map((data) => (
+            <div
+              className='flex flex-col items-center gap-4 m-6 p-8 bg-gray-100 rounded-md'
+              key={data.id}
+            >
+              <div className='w-[250px] h-[250px]'>
+                <Image
+                  className='h-full w-full object-cover rounded-md'
+                  src={data.business_img}
+                  alt='Business'
+                  width={250}
+                  height={250}
+                  unoptimized
+                />
+              </div>
+              <p className='font-bold text-gray-80'>{data.business_name}</p>
+              <Link
+                className='text-gray-800 text-center text-sm bg-white p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110 rounded-md'
+                href={`/foods/${data.id}`}
+              >
+                Discover More!
+              </Link>
+            </div>
+          ))}
+        {show === 1 &&
+          Restaurant.map((data) => (
+            <div
+              className='flex flex-col items-center gap-4 m-6 p-6 bg-gray-100'
+              key={data.id}
+            >
+              <div className='w-[250px] h-[250px]'>
+                <Image
+                  className='h-full w-full object-cover'
+                  src={data.business_img}
+                  alt='Business'
+                  width={250}
+                  height={250}
+                  unoptimized
+                />
+              </div>
+              <h1 className='text-gray-800 bg-white'>{data.business_name}</h1>
+              <Link
+                className='text-gray-800 text-center text-sm bg-white p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110'
+                href={`/foods/${data.id}`}
+              >
+                Discover More!
+              </Link>
+            </div>
+          ))}
+        {show === 2 &&
+          cafe.map((data) => (
+            <div
+              className='flex flex-col items-center gap-4 m-6 p-6 bg-gray-100'
+              key={data.id}
+            >
+              <div className='w-[250px] h-[250px]'>
+                <Image
+                  className='h-full w-full object-cover'
+                  src={data.business_img}
+                  alt='Business'
+                  width={250}
+                  height={250}
+                  unoptimized
+                />
+              </div>
+              <h1 className='text-gray-800 bg-white'>{data.business_name}</h1>
+              <Link
+                className='text-gray-800 text-center text-sm bg-white p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110'
+                href={`/foods/${data.id}`}
+              >
+                Discover More!
+              </Link>
+            </div>
+          ))}
+        {show === 3 &&
+          Bars.map((data) => (
+            <div
+              className='flex flex-col items-center gap-4 m-6 p-6 bg-gray-100'
+              key={data.id}
+            >
+              <div className='w-[250px] h-[250px]'>
+                <Image
+                  className='h-full w-full object-cover'
+                  src={data.business_img}
+                  alt='Business'
+                  width={250}
+                  height={250}
+                  unoptimized
+                />
+              </div>
+              <h1 className='text-gray-800 bg-white'>{data.business_name}</h1>
+              <Link
+                className='text-gray-800 text-center text-sm bg-white p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110'
+                href={`/foods/${data.id}`}
+              >
+                Discover More!
+              </Link>
+            </div>
+          ))}
+      </div>
     </>
   );
 }
